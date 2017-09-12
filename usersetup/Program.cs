c18f158005dcf98b348c88cf1b2d33a70ef31760
Lorence <hernandez.lorence@yahoo.com>
2017-09-12 23:27:32 +0800

    program for setting up an account

---
+++ usersetup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace UserSetup
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+
+                if (args[0] == "dentalrecord")
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Setup());
+                }
+            }
+        }
+    }
+}
---
+++ usersetup/Setup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+namespace UserSetup
+{
+    public partial class Setup : Form
+    {
+        public Setup()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text.Length == 0 || txtPassword.Text.Length == 0)
+            {
+                MessageBox.Show("fill up");
+                return;
+            }
+            Configuration config = new Configuration();
+            config.username = txtUser.Text;
+            config.password = txtPassword.Text;
+            string json = JsonConvert.SerializeObject(config);
+            File.WriteAllText("usersetup.json", json);
+            this.Close();
+        }
+
+        private void Setup_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Configuration
+    {
+        public string username;
+        public string password;
+    }
+}
