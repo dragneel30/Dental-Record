8dbcd0e0ea06277f4d1c66913c068c6654406885
Lorence <hernandez.lorence@yahoo.com>
2017-09-12 23:26:25 +0800

    program for configurating database credential.

---
+++ databaseconfigurator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DatabaseConfigurator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "dentalrecord")
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new maincs());
+                }
+            }
+        }
+    }
+}
---
+++ databaseconfigurator/maincs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+namespace DatabaseConfigurator
+{
+    public partial class maincs : Form
+    {
+        public maincs()
+        {
+            InitializeComponent();
+        }
+
+        class Conf
+        {
+            public Conf(string db, string h, string u, string p)
+            {
+                database = db;
+                host = h;
+                username = u;
+                password = p;
+            }
+            public string database;
+            public string host;
+            public string username;
+            public string password;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Length == 0 || txtHost.Text.Length == 0 )
+            {
+                MessageBox.Show("no input can be empty");
+            }
+            else
+            {
+
+               DialogResult result = MessageBox.Show("Are you sure?", "Warning", MessageBoxButtons.YesNo);
+               if (result == DialogResult.Yes)
+               {
+                   Conf dbconf = new Conf("dental", txtHost.Text, txtUsername.Text, txtPassword.Text);
+                   string serializedobj = JsonConvert.SerializeObject(dbconf);
+                   File.WriteAllText("dbconf.json", serializedobj);
+                   this.Close();
+               }
+            }
+        }
+    }
+}
